b30225942ab1b8f176566e539c888d9158c612ec
Abhishek Chaudhary <chaudha4@hotmail.com>
2020-08-05 17:06:06 -0400

    Move Admin out of Singer container

--- src/views/Sidenav.js
+++ src/views/Sidenav.js
@@ -1,35 +1,32 @@
 
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 /* Using Link on sidenav to link to React components on the main panel */
 
-export default function Sidenav({artists}) {
+export default function Sidenav({ artists }) {
 
-  
+  return (
+    <div>
+      <div className="sidenav">
+        <h2>Singers</h2>
+        {artists.map(artist => (
+          <li key={artist}>
+            <Link to={artist}>
+              <small>{artist}</small>
+            </Link>
+          </li>
+        ))}
+      </div>
+      <div className="sidenav">
 
-    return (
+          <Link to="/Add">
+            <small>Add New User</small>
+          </Link>
 
-        <div className="sidenav">
-          <h2>Singers</h2>
-          
-          {artists.map(artist => (
-            <li key={artist}>
-              <Link to={artist}>
-                <small>{artist}</small>
-              </Link>
-            </li>
-          ))}
+      </div>
+    </div>
 
-          <h2>Admin</h2>
-            <li>
-                <Link to="/Add">
-                    <small>Add New User</small>
-                </Link>
-            </li>         
-        </div>
-
-
-    );
+  );
 
 }
