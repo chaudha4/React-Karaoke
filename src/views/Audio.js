a0dcdd1901390b87de44a172d4c2657dcff10890
Abhishek Chaudhary <chaudha4@hotmail.com>
2020-08-05 16:11:51 -0400

    Mobile friendly changes.

--- src/views/Audio.js
+++ src/views/Audio.js
@@ -1,18 +1,18 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import * as model from './../models/ArtistModel';
 
-export default function Audio({url, name, artist, trigger}) {
+export default function Audio({ url, name, artist, trigger }) {
 
     const [checked, setChecked] = useState(false);
 
     console.log("Entering Audio for %s", name);
 
-    useEffect( () => {
+    useEffect(() => {
         console.log("Audio::useEffect Received a trigger from Parent ", trigger, checked);
         async function DoAsync() {
             console.log("Audio::useEffect Deleting Song %s", name);
-            if(checked) {
+            if (checked) {
                 await model.deleteSong(artist, name);
                 setChecked(false);
             }
@@ -21,22 +21,23 @@ export default function Audio({url, name, artist, trigger}) {
         DoAsync();
     }, [trigger, artist, checked, name]); // Only process if trigger changed
 
-    useEffect( () => {
+    useEffect(() => {
         console.log("");
     }, []); // Only process if trigger changed    
 
     function onChange(e) {
         //console.log(e.target);
         setChecked(!checked);
-    }    
+    }
 
     return (
         <div className='audioplayer'>
             <div className='audioplayer--title'>
-                <p className="center">{name}</p>
                 <input type='checkbox' checked={checked} onChange={onChange} />
-            </div>            
-            
+                <p className="center">{name}</p>
+
+            </div>
+
             <audio controls preload="auto" src={url}>
                 Your browser does not support the <code>audio</code> element.
             </audio>
